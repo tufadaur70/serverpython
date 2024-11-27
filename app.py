14648ec68e5d7ba67d349eb6417b7c520fd466bc
tufadaur70 <157790985+tufadaur70@users.noreply.github.com>
2024-11-27 14:50:31 +0100

    a

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
-import RPi.GPIO as GPIO
-import board
-import adafruit_dht
+from flask import Flask, render_template  , request # type: ignore
+import RPi.GPIO as GPIO # type: ignore
+import time
+import board # type: ignore
+import adafruit_dht # type: ignore
  
 app = Flask(__name__)
  
 GPIO.setmode(GPIO.BCM)
-led1 = 21 
-led2 = 20
+
 sensor = adafruit_dht.DHT22(board.D4, use_pulseio=False)
  
 # Set each pin as an output and make it low:
-GPIO.setup(led1, GPIO.OUT)
-GPIO.setup(led2, GPIO.OUT)
+
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, GPIO.LOW)
  
 @app.route("/")
  
@@ -21,43 +22,37 @@ def main():
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
  
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/<pin>/<action>")
-def action(pin, action):
+@app.route("/caldaia" , methods=['POST'] )
+def caldaia():
+  if request.method == 'POST':
+      print(request.form)
+      if request.form.get('stato') == 'ON':
+          GPIO.output(21, GPIO.HIGH)
+      if request.form.get('stato') == 'OFF':
+          GPIO.output(21, GPIO.LOW)
+      return('', 204)
+
+
+@app.route('/temperatura')
+def temperatura():   
    temperature = ''
    humidity = ''
-   if pin == "pin1" and action == "on":
-      GPIO.output(led1, GPIO.HIGH)
-    
-   if pin == "pin1" and action == "off":
-      GPIO.output(led1, GPIO.LOW)
-    
-   if pin == "pin2" and action == "on":
-      GPIO.output(led2, GPIO.HIGH)
-    
-   if pin == "pin2" and action == "off":
-      GPIO.output(led2, GPIO.LOW)
- 
-   if pin == "dhtpin" and action == "get":
-      
-      temp = sensor.temperature
-      humi = sensor.humidity
-      try:
+   
+   temp = sensor.temperature
+   humi = sensor.humidity
+   try:
         humi = '{0:0.1f}' .format(humi)
         temp = '{0:0.1f}' .format(temp)
         temperature = 'Temperature: ' + temp 
         humidity =  'Humidity: ' + humi
-      except:
+   except:
         temperature = 'Non Letto'
         humidity =  'Non Letto'
          
  
-   templateData = {
-   'temperature' : temperature,
-   'humidity' : humidity
-   }
+   
  
-   return render_template('main.html', **templateData)
+   return (temperature , 201)
  
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
